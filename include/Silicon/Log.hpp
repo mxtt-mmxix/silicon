05c4df0445a523f7928790c04309ee39e5327b38
Matthew McCall <46231996+mxtt-mmxix@users.noreply.github.com>
2022-05-28 14:04:51 -0400

    Update copyright

--- include/Silicon/Log.hpp
+++ include/Silicon/Log.hpp
@@ -1,6 +1,30 @@
-//
-// Created by Matthew McCall on 5/27/22.
-//
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2022, Matthew McCall
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef SILICON_LOG_HPP
 #define SILICON_LOG_HPP
@@ -11,19 +35,28 @@
 
 namespace Si {
 
+/**
+ * A log entry. This holds information to be logged independent of the backend.
+ *
+ * This allows the same information to be logged to multiple backends, e.g.: stdout or a file.
+ */
 struct LogEntry {
+
+    /**
+     * The type of log
+     */
     enum class Type {
-        Engine,
-        Client
+        Engine,    /**< Logs from the engine. Please refrain from using in your application. */
+        Client    /**< Logs from the client. Use this to for your logging. */
     };
 
     enum class Level {
-        Trace,
-        Information,
-        Debug,
-        Warning,
-        Error,
-        Critical
+        Trace,          /**< Verbose information, not informative to the end-user. */
+        Information,    /**< Information that maybe relevant to the end user. */
+        Debug,          /**< Debug, technical information. */
+        Warning,        /**< Information that could suggest an error but does not affect application. */
+        Error,          /**< Information that may affect the engine and may cause a crash. */
+        Critical        /**< Information pertaining to a crash. */
     };
 
     Type type = Type::Client;
--- include/Silicon/Node.hpp
+++ include/Silicon/Node.hpp
@@ -1,6 +1,30 @@
-//
-// Created by Matthew McCall on 5/27/22.
-//
+/*
+ * BSD 2-Clause License
+ *
+ * Copyright (c) 2022, Matthew McCall
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #ifndef SILICON_NODE_HPP
 #define SILICON_NODE_HPP
@@ -11,6 +35,14 @@
 
 namespace Si {
 
+/**
+ * A Node is the building block of your game.
+ *
+ * Nodes allow you to define your application declaratively. Nodes can be attached to other nodes to form a node tree.
+ * While Nodes are designed to be created and attached declaratively, they can created and attached imperatively as
+ * well. You may inherit the nodes to provide your own functionality. All Nodes can be attached to another
+ * declaratively, but not every Node needs or should allow child Nodes to be attached declaratively.
+ */
 class Node {
 public:
     using Graph = Si::Graph<Node*, GraphList>;
