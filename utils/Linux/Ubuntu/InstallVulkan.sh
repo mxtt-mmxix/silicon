13645faf4e4e906f2fa3809615be4fdfb38e1083
Matthew McCall <46231996+mxtt-mmxix@users.noreply.github.com>
2022-05-30 18:29:22 -0400

    No need to reinvent the wheel

--- utils/Linux/Ubuntu/InstallVulkan.sh
+++ utils/Linux/Ubuntu/InstallVulkan.sh
@@ -2,8 +2,4 @@
 
 curl -fsSL http://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
 sudo curl -fsSLo /etc/apt/sources.list.d/lunarg-vulkan-focal.list http://packages.lunarg.com/vulkan/lunarg-vulkan-focal.list
-sudo apt-get update -y && sudo apt-get install \
-libglm-dev libxcb-dri3-0 libxcb-present0 libpciaccess0 \
-libpng-dev libxcb-keysyms1-dev libxcb-dri3-dev libx11-dev \
-libmirclient-dev libwayland-dev libxrandr-dev libxcb-randr0-dev \
-libxcb-ewmh-dev libx11-xcb-dev liblz4-dev libxcursor-dev vulkan-sdk
+sudo apt-get update -y && sudo apt-get install libsdl2-dev vulkan-sdk
