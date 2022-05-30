381854af6a3a3b8c75a75d3d3720b4c0b8272740
Matthew McCall <46231996+mxtt-mmxix@users.noreply.github.com>
2022-05-30 15:49:41 -0400

    Fixing Ubuntu again

--- utils/Linux/Ubuntu/InstallVulkan.sh
+++ utils/Linux/Ubuntu/InstallVulkan.sh
@@ -6,4 +6,4 @@ sudo apt-get update -y && sudo apt-get install \
 libglm-dev libxcb-dri3-0 libxcb-present0 libpciaccess0 \
 libpng-dev libxcb-keysyms1-dev libxcb-dri3-dev libx11-dev \
 libmirclient-dev libwayland-dev libxrandr-dev libxcb-randr0-dev \
-libxcb-ewmh-dev libx11-xcb-dev liblz4-dev libzstd1-dev vulkan-sdk
+libxcb-ewmh-dev libx11-xcb-dev liblz4-dev vulkan-sdk
