2d44faada355710042a2e09afa1a2783ecc22e9b
Matthew McCall <46231996+mxtt-mmxix@users.noreply.github.com>
2022-06-15 12:24:23 -0400

    Better Emscripten support

--- test/HelloWorld/HelloWorld.cmake
+++ test/HelloWorld/HelloWorld.cmake
@@ -1,7 +1,18 @@
 add_executable(HelloWorld test/HelloWorld/HelloWorld.cpp)
 target_link_libraries(HelloWorld Silicon)
 
+if (BUILD_EMSCRIPTEN)
+    if (DEFINED ENV{EMSDK_NODE})
+        add_test(
+                NAME HelloWorld
+                COMMAND "$ENV{EMSDK_NODE}" "HelloWorld.js"
+                PROPERTY PASS_REGULAR_EXPRESSION "Hello, World!$")
+    else()
+        message(FATAL_ERROR "-DBUILD_EMSCRIPTEN is enabled but the Emscripten SDK Node.JS was not found. Please set the EMSDK_NODE environment variable.")
+    endif()
+else()
 add_test(
         NAME HelloWorld
         COMMAND HelloWorld
         PROPERTY PASS_REGULAR_EXPRESSION "Hello, World!$")
+endif()
--- test/NodeTree/NodeTree.cmake
+++ test/NodeTree/NodeTree.cmake
@@ -1,6 +1,16 @@
 add_executable(NodeTree test/NodeTree/NodeTree.cpp)
 target_link_libraries(NodeTree Silicon)
 
-add_test(
-        NAME NodeTree
-        COMMAND NodeTree)
+if (BUILD_EMSCRIPTEN)
+    if (DEFINED ENV{EMSDK_NODE})
+        add_test(
+                NAME NodeTree
+                COMMAND "$ENV{EMSDK_NODE}" "NodeTree.js")
+    else()
+        message(FATAL_ERROR "-DBUILD_EMSCRIPTEN is enabled but the Emscripten SDK Node.JS was not found. Please set the EMSDK_NODE environment variable.")
+    endif()
+else()
+    add_test(
+            NAME NodeTree
+            COMMAND NodeTree)
+endif()
